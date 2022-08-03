8d84ea592a7b1ceeb2d0ee7f5d3d0e4bdebcd76c
Arslan Ali <arslan@turingtechnologies.org>
2022-08-03 12:24:38 +0500

    Components TypeA Added

---
+++ types/modal.ts
@@ -0,0 +1,3 @@
+import { ModalProps as AntModalProps } from 'antd/es/modal';
+
+export type ModalProps = AntModalProps;
---
+++ types/table.ts
@@ -0,0 +1,3 @@
+import { TableProps as AntTableProps } from 'antd/es/table';
+export type Any = any;
+export type TableProps = AntTableProps<Any>;
